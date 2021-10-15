2451f9561b56dc4fafecbbde2f0784b75d769415
Juicestus <jus@gtsbr.org>
2021-10-15 11:39:44 -0500

    :q

---
+++ CS-1-Labs/Pr0210.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pr0208 {
+	public static String removeFirst(char c, String s) {
+		return s.substring(0, s.indexOf(c)) 
+				+ s.substring(s.indexOf(c) + 1);
+	}
+    
+    public static void main(String[] args) {    
+        Scanner kb = new Scanner(System.in);    
+        System.out.print("Give a string with at least 3 capital F's: ");    
+        String line = kb.nextLine(); 
+		
+		line = removeFirst('F', line);
+		line = removeFirst('F', line);
+		line = removeFirst('F', line);
+		System.out.println("FFF" + line);
+    }
+}
