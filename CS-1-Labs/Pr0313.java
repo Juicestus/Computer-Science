6742a668396164b70811390ec34a512b5b473e50
Juicestus <jus@gtsbr.org>
2021-11-30 20:17:24 -0600

    Didnt return correct datatype on lab

--- CS-1-Labs/Pr0313.java
+++ CS-1-Labs/Pr0313.java
@@ -1,14 +1,17 @@
 public class Pr0313 {
-    public static String oddZero(double n) {
-        if (n % 2 == 1)
-            return "" + ("" + n).charAt(2) + ("" + n).charAt(1) + ("" + n).charAt(0);
-        else
-            return ("" + n).charAt(1) + "0" + ("" + n).charAt(0) + "0" + ("" + n).charAt(2);
-    }
+	public static int oddZero(double n) {
+		int ones = (int) n % 10;
+		int tens = (int) Math.floor((n % 100) / 10);
+		int hunds = (int) Math.floor(n / 100);
+		
+		if (n % 2 == 0) 
+			return hunds * 10000 + tens * 100 + ones;
+		return ones * 100 + tens * 10 + hunds;
+	}
 
-    public static void main(String[] args) {
-        System.out.println(oddZero(
-            MyLibrary.getDouble("Give me a 3 digit number: ")
-        ));
-    }
+	public static void main(String[] args) {
+		System.out.println(oddZero(
+			MyLibrary.getDouble("Give me a 3 digit number: ")
+		));
+	}
 }
