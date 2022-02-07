a2efe63e06ca6ba0801148e520ef37374a468a98
Juicestus <jus@gtsbr.org>
2022-02-06 21:25:21 -0600

    Pr0433

---
+++ CS-1-Labs/Pr0433.java
@@ -0,0 +1,12 @@
+public class prr0433 {
+	public static boolean palindrome(String w) {
+		for (int i = 0, j = w.length() - 1; i <= j;)
+			if (w.charAt(i++) != w.charAt(j--))
+				return false;
+		return true;
+	}
+
+    public static void main(String[] args) {
+		System.out.println(palindrome(MyLibrary.getWord("Word:")));
+   	} 
+}
