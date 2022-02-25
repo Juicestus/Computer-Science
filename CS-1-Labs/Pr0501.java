0a3da4a1c325e5d910962510637eaaf741fc5ed8
Juicestus <jus@gtsbr.org>
2022-02-24 20:37:41 -0600

    Pr0501

---
+++ CS-1-Labs/Pr0501.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pr0501 {
+    public static void main(String[] args) {
+		String input = MyLibrary.getLine("In:");
+		Scanner chopper = new Scanner(input);
+		double sum = 0, n = 0;
+		for ( ; chopper.hasNextDouble(); n++)
+			sum += chopper.nextDouble();
+		System.out.printf("Sum: %f, Avg: %f\n", 
+				sum, sum / n);
+    }
+}
