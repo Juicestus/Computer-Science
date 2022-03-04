b337a7e3b6b4e8513169dd0745c6cc3fd2ed200b
Juicestus <jus@gtsbr.org>
2022-03-04 06:13:54 -0600

    FileReader02

---
+++ CS-1-Labs/FileRead02.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class FileRead02 {
+    public static void main(String[] args) throws IOException {
+		
+		Scanner fileReader = new Scanner(new File("dat/second.dat"));
+		int sum = 0;
+		while (fileReader.hasNextInt())
+			sum += fileReader.nextInt();
+		System.out.println(sum);
+
+    }
+}
