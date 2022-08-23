b24d608db2b4b069df067df4524d25ec5b351609
Juicestus <jus@gtsbr.org>
2022-08-23 11:38:19 -0500

    Matrix runner labs (pt2)

---
+++ CS-AP/MatrixRunner3.java
@@ -0,0 +1,40 @@
+public class MatrixRunner3 {
+    public static void main(final String[] args)
+    {
+        final int[][] matNums = new int[][]
+        {
+            {0, 6, 3, 7},
+            {4, 5, 9, 0},
+            {-2, 15, 1, 2}
+        };
+
+        final int[][] testCases = new int[][] 
+        {
+            {3, 2},
+            {6, 28},
+            {1, 0}
+        };
+
+        for (final int[] testCase : testCases) {
+            final int result = sumCount(matNums, testCase[0]);
+            final boolean passed = result == testCase[1];
+            System.out.printf("f(%d) = %d %s %d [%s]\n", 
+                    testCase[0], result, passed ? "==" : "!=", testCase[1], passed ? "PASS" : "FAIL");
+        }
+    }    
+
+    public static int sumCount(final int[][] matNums, final int n) {
+        int sum = 0, count = 0;
+        for (int col = 0; col < matNums[0].length; col++) {
+            final boolean down = col % 2 == 0;
+            for (int row = down ? 0 : matNums.length - 1;
+                    down ? row < matNums.length : row >= 0;
+                    row += down ? 1 : -1) 
+            {
+                if (count++ >= n) return sum;
+                sum += matNums[row][col];
+            }
+        }
+        return sum;
+    }
+} 
